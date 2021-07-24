15c1bfb2e6e153044e1492782c0b90eeb802cfb0
Grzegorz Fojcik <gfojcik@incuvo.com>
2021-07-24 17:03:07 +0200

    Lobby photon init

--- HexStartegy/Assets/Scripts/SceneChanger.cs
+++ HexStartegy/Assets/Scripts/SceneChanger.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class SceneChanger : MonoBehaviour
 {
     private void Awake()
